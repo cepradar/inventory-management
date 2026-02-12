aee56f2748f9b766c95d9c4f8150804d08e06f8a
Carlos Prada <carlosprada95@gmail.com>
2026-02-12 11:06:34 -0500

    ajustes al gitignore para mejorar versionamiento

--- inventory-frontend/src/components/utils/axiosConfig.jsx
+++ inventory-frontend/src/components/utils/axiosConfig.jsx
@@ -1,7 +1,7 @@
 // src/api/axiosConfig.js
 import axios from 'axios';
 
-
+//-------------------------
 const API_BASE =
   import.meta.env.VITE_API_BASE_URL ||
   `${window.location.protocol}//${window.location.hostname}:8080`;
