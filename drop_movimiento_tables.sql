eead41adf2d339d62930ef6f9925f38a19c5ccfd
Carlos Prada <carlosprada95@gmail.com>
2026-02-12 00:14:25 -0500

    company initilizer

--- drop_movimiento_tables.sql
+++
@@ -1,9 +0,0 @@
--- Script para eliminar tablas movimiento_* de la base de datos
-
--- Primero verificar si existen y luego eliminar
-DROP TABLE IF EXISTS movimiento_producto CASCADE;
-DROP TABLE IF EXISTS movimiento_inventario CASCADE;
-DROP TABLE IF EXISTS movimientos_inventario CASCADE;
-
--- Mensaje de confirmación (comentario)
--- Tablas movimiento_* eliminadas exitosamente
