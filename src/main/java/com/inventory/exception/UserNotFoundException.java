6c1640fd7343eeff5fb6135990048b505693149d
Carlos Prada <carlosprada95@gmail.com>
2026-02-12 11:23:47 -0500

    asdf

--- src/main/java/com/inventory/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.inventory.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
